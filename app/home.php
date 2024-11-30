cd33e2338bde1c44ad70ae18b4606befc1179fe5
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-11-30 11:17:39 -0500

    Added Jquery and Footer with scripts

--- app/home.php
+++ app/home.php
@@ -1,9 +1,7 @@
-<?php ?>
-
 
 <div class="home-container">
   <h1>Welcome to the Movies API</h1>
-</div>
+
 <div class="cards">
 <div class="card">
     <div class="title">Lorem</div>
@@ -17,3 +15,4 @@
     <div class="studio">By: Ipsum</div>
     <div class="r-date">Released On: Release Date</div></div>
     </div>
+</div>
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
 require __DIR__ . '/app/nav.php';
  require __DIR__ . '/app/home.php';
+ require __DIR__ . '/app/footer.php';
 ?>
